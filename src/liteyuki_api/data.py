8c59616e15c3d6e33ccfce9cacff141ac2adb9b2
KamiDev <79104275+snowyfirefly@users.noreply.github.com>
2022-12-21 10:08:10 +0800

    Initial commit

--- src/liteyuki_api/data.py
+++
@@ -1,8 +0,0 @@
-import pymongo
-
-
-mc = pymongo.MongoClient("mongodb://localhost:27017")
-mydb = mc["runoobdb"]
-mndb = mc["liteyuki"]
-mncol = mndb["users"]
-mncol.insert_one({"quid": 114514, "luid": 1919810})
