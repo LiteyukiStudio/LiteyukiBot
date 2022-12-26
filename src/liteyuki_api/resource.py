a55e1c21d16c5f97969fce82b9970ca373e5362a
KamiDev <79104275+snowyfirefly@users.noreply.github.com>
2022-12-26 10:27:52 +0800

    Merge remote-tracking branch 'origin/master'

--- src/liteyuki_api/resource.py
+++ src/liteyuki_api/resource.py
@@ -5,15 +5,15 @@ from .config import Path
 
 class Font:
 
-    MiSans_Bold = os.path.join(Path.res, "textures/fonts/MiSans-Bold.ttf")
-    MiSans_Demibold = os.path.join(Path.res, "textures/fonts/MiSans-Demibold.ttf")
-    MiSans_ExtraLight = os.path.join(Path.res, "textures/fonts/MiSans-ExtraLight.ttf")
-    MiSans_Heavy = os.path.join(Path.res, "textures/fonts/MiSans-Heavy.ttf")
-    MiSans_Light = os.path.join(Path.res, "textures/fonts/MiSans-Light.ttf")
-    MiSans_Medium = os.path.join(Path.res, "textures/fonts/MiSans-Medium.ttf")
-    MiSans_Normal = os.path.join(Path.res, "textures/fonts/MiSans-Normal.ttf")
-    MiSans_Regular = os.path.join(Path.res, "textures/fonts/MiSans-Regular.ttf")
-    MiSans_Semibold = os.path.join(Path.res, "textures/fonts/MiSans-Semibold.ttf")
-    MiSans_Thin = os.path.join(Path.res, "textures/fonts/MiSans-Thin.ttf")
+    MiSans_Bold = os.path.join(Path.res, "fonts/MiSans-Bold.ttf")
+    MiSans_Demibold = os.path.join(Path.res, "fonts/MiSans-Demibold.ttf")
+    MiSans_ExtraLight = os.path.join(Path.res, "fonts/MiSans-ExtraLight.ttf")
+    MiSans_Heavy = os.path.join(Path.res, "fonts/MiSans-Heavy.ttf")
+    MiSans_Light = os.path.join(Path.res, "fonts/MiSans-Light.ttf")
+    MiSans_Medium = os.path.join(Path.res, "fonts/MiSans-Medium.ttf")
+    MiSans_Normal = os.path.join(Path.res, "fonts/MiSans-Normal.ttf")
+    MiSans_Regular = os.path.join(Path.res, "fonts/MiSans-Regular.ttf")
+    MiSans_Semibold = os.path.join(Path.res, "fonts/MiSans-Semibold.ttf")
+    MiSans_Thin = os.path.join(Path.res, "fonts/MiSans-Thin.ttf")
 
-    HYWH_85w = os.path.join(Path.res, "textures/fonts/hywh-85w.ttf")
+    HYWH_85w = os.path.join(Path.res, "fonts/hywh-85w.ttf") 
--- src/liteyuki_plugins/liteyuki_pluginmanager/__init__.py
+++ src/liteyuki_plugins/liteyuki_pluginmanager/__init__.py
@@ -11,7 +11,8 @@ __plugin_meta__ = PluginMetadata(
           "•「help」获取插件列表\n"
           "•「help插件名」获取插件使用方法\n"
           "•「启用/停用xxx」插件开关\n"
-          "•「添加插件元数据 name description usage extra」\n",
+          "•「添加插件元数据 name description usage extra」\n"
+          "•「隐藏插件xxx」将部分后台插件隐藏起来，可使用「全部插件」查看所有插件",
     extra={
         "force_enable": True,
         "liteyuki_resource": resource,
