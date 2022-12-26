a62548f630108ddfd0fd8ccef565d3016737486f
KamiDev <79104275+snowyfirefly@users.noreply.github.com>
2022-12-26 08:42:40 +0800

    Merge remote-tracking branch 'origin/master'

--- src/liteyuki_plugins/liteyuki_pluginmanager/resource.py
+++ src/liteyuki_plugins/liteyuki_pluginmanager/resource.py
@@ -1,4 +1,4 @@
 resource = {
-    "src/resource/textures/liteyuki/state_on.png": "https://raw.kgithubusercontent.com/snowyfirefly/Liteyuki-Resource/master/textures/liteyuki/state_on.png",
-    "src/resource/textures/liteyuki/state_off.png": "https://raw.kgithubusercontent.com/snowyfirefly/Liteyuki-Resource/master/textures/liteyuki/state_off.png"
+    "src/resource/textures/liteyuki/state_on.png": "https://ghproxy.com/https://raw.githubusercontent.com/snowyfirefly/Liteyuki-Resource/master/textures/liteyuki/state_on.png",
+    "src/resource/textures/liteyuki/state_off.png": "https://ghproxy.com/https://raw.githubusercontent.com/snowyfirefly/Liteyuki-Resource/master/textures/liteyuki/state_off.png"
 }
