41c981d9f569c7252fb7383adf36fb86e15916d6
KamiDev <79104275+snowyfirefly@users.noreply.github.com>
2022-12-27 23:35:40 +0800

    Merge remote-tracking branch 'origin/master'

--- src/liteyuki_plugins/liteyuki_base/utils.py
+++ src/liteyuki_plugins/liteyuki_base/utils.py
@@ -1,2 +1,10 @@
+from nonebot import require
+
+require("nonebot_plugin_reboot")
+from nonebot_plugin_reboot import Reloader
+
+ # 可选参数 5秒后触发重启
+
+
 def restart_bot():
-    pass
+    Reloader.reload()
