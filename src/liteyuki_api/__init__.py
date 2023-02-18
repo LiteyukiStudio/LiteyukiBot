18250cb56f5f78d4e6666f38b61b88e55daf324e
SnowyKami <snowykami@outlook.com>
2023-02-18 22:37:21 +0800

    增加法语支持

---
+++ src/liteyuki_api/__init__.py
@@ -0,0 +1,10 @@
+from nonebot.plugin import PluginMetadata
+
+__plugin_meta__ = PluginMetadata(
+    name="轻雪API",
+    description="轻雪机器人api，由机器人调用",
+    usage="若你不是开发者，无需了解其用途",
+    extra={
+        "liteyuki_plugin":True
+    }
+)
