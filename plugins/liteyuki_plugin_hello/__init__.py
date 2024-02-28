ebe61e187ece210c98dc2f840f5422450ae62403
snowy <snowykami@outlook.com>
2024-02-29 01:09:16 +0800

    first commit

--- plugins/liteyuki_plugin_hello/__init__.py
+++
@@ -1,9 +0,0 @@
-from nonebot import on_command
-from nonebot.rule import to_me
-
-hello = on_command('hello', aliases={'你好'}, rule=to_me())
-
-
-@hello.handle()
-async def handle_first_receive(bot, event, state):
-    await hello.finish('Hello, world!')
