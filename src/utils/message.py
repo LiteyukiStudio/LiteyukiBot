ab5dc2200ae6b35f9aa8c50f9167f88de98a6c79
snowyServer <snowykami@outlook.com>
2024-03-19 22:43:55 +0800

    feat: 添加了对markdown的简单封装

--- src/utils/message.py
+++ src/utils/message.py
@@ -2,11 +2,12 @@ import nonebot
 from nonebot.adapters.onebot import v11, v12
 from typing_extensions import Any
 
+from .tools import de_escape
 from .typing import T_Bot
 
 
 async def send_markdown(markdown: str, bot: T_Bot, message_type: str, session_id: str) -> dict[str, Any]:
-    markdown = markdown.replace("\n", r"\n").replace("\"", r'\\\"')
+    formatted_md = de_escape(markdown).replace("\n", r"\n").replace("\"", r'\\\"')
     forward_data = await bot.call_api(
         api="send_private_forward_msg",
         user_id=bot.self_id,
@@ -20,7 +21,7 @@ async def send_markdown(markdown: str, bot: T_Bot, message_type: str, session_id
                         {
                             "type": "markdown",
                             "data": {
-                                "content": '{"content":"%s"}' % markdown
+                                "content": '{"content":"%s"}' % formatted_md
                             }
                         }
                     ]
@@ -29,18 +30,28 @@ async def send_markdown(markdown: str, bot: T_Bot, message_type: str, session_id
 
         ]
     )
-    data = await bot.send_msg(
-        message_type=message_type,
-        message=[
-            v11.MessageSegment(
-                type="longmsg",
-                data={
-                    "id": forward_data["forward_id"]
-                }
-            )
-        ],
-        user_id=session_id if message_type == "private" else None,
-        group_id=session_id if message_type == "group" else None
-    )
-    nonebot.logger.info("已发送md：%s" % forward_data["forward_id"])
+    try:
+        data = await bot.send_msg(
+            message_type=message_type,
+            message=[
+                v11.MessageSegment(
+                    type="longmsg",
+                    data={
+                        "id": forward_data["forward_id"]
+                    }
+                ),
+            ],
+
+
+        )
+    except Exception as e:
+        nonebot.logger.warning("send_markdown error, send as plane text: %s", e)
+        data = await bot.send_msg(
+            message_type=message_type,
+            message=markdown,
+            user_id=session_id if message_type == "private" else None,
+            group_id=session_id if message_type == "group" else None
+
+
+        )
     return data
