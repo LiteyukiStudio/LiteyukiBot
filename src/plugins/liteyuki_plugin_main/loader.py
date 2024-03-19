958591062382e8c668fd2a21c821e69bbdeccb14
snowy <snowykami@outlook.com>
2024-03-19 15:58:02 +0800

    feat: 添加了网页监控面板

--- src/plugins/liteyuki_plugin_main/loader.py
+++ src/plugins/liteyuki_plugin_main/loader.py
@@ -5,10 +5,12 @@ import nonebot.plugin
 from src.utils.language import load_from_dir
 from src.utils.resource import load_resource_from_dir
 
-PLUGIN_NAME = os.path.basename(os.path.dirname(__file__))
+THIS_PLUGIN_NAME = os.path.basename(os.path.dirname(__file__))
 RESOURCE_PATH = "src/resources"
 load_resource_from_dir(RESOURCE_PATH)
 
 for plugin_dir in os.listdir("src/plugins"):
-    if plugin_dir != PLUGIN_NAME:
+    if plugin_dir != THIS_PLUGIN_NAME:
         nonebot.plugin.load_plugin(f"src.plugins.{plugin_dir}")
+
+nonebot.plugin.load_plugins("plugins")
