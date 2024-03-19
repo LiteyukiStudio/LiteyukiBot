0bf56f79f14d320538a23623c6d9d4840bd4740c
snowyServer <snowykami@outlook.com>
2024-03-19 22:06:30 +0800

    feat: 添加了对markdown的简单封装

--- src/utils/message.py
+++ src/utils/message.py
@@ -31,13 +31,14 @@ async def send_markdown(markdown: str, bot: T_Bot, message_type: str, session_id
     )
     data = await bot.send_msg(
         message_type=message_type,
-        message=[{
-                "type": "longmsg",
-                "data": {
+        message=[
+            v11.MessageSegment(
+                type="longmsg",
+                data={
                     "id": forward_data["forward_id"]
                 }
-    }],
-
+            )
+        ],
         user_id=session_id if message_type == "private" else None,
         group_id=session_id if message_type == "group" else None
     )
