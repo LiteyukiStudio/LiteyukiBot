58ab62c03b4ebb3f6212be148f3b81982f490856
snowy <snowykami@outlook.com>
2024-03-27 23:32:44 +0800

    docs: 使用vuepress重新构建文档

--- docs/src/.vuepress/config.ts
+++ docs/src/.vuepress/config.ts
@@ -2,7 +2,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/",
+  base: "/docs/src/",
 
   lang: "zh-CN",
   title: "文档演示",
