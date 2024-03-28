47f00c48c57646c591ce13a05e464b222b463464
snowy <snowykami@outlook.com>
2024-03-28 10:50:19 +0800

    docs: vuepress构建文档

---
+++ docs/.vuepress/sidebar.ts
@@ -0,0 +1,25 @@
+import { sidebar } from "vuepress-theme-hope";
+
+export default sidebar({
+  "/": [
+    "",
+    {
+      text: "案例",
+      icon: "laptop-code",
+      prefix: "demo/",
+      link: "demo/",
+      children: "structure",
+    },
+    {
+      text: "文档",
+      icon: "book",
+      prefix: "guide/",
+      children: "structure",
+    },
+    {
+      text: "幻灯片",
+      icon: "person-chalkboard",
+      link: "https://plugin-md-enhance.vuejs.press/zh/guide/content/revealjs/demo.html",
+    },
+  ],
+});
