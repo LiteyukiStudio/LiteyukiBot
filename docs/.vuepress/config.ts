d44b5ea1432ae6a93d09301f20348e66e4c86669
snowy <snowykami@outlook.com>
2024-03-28 08:33:38 +0800

    docs: vuepress构建文档

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -2,7 +2,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/",
+  base: "/LiteyukiBot",
 
   lang: "zh-CN",
   title: "文档演示",
