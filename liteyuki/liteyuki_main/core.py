b611ec1714ad32c3a4d631b6bb233de3e17386cb
snowy <snowykami@outlook.com>
2024-04-01 12:33:35 +0800

    feat: 可以设置发送高清大图

--- liteyuki/liteyuki_main/core.py
+++ liteyuki/liteyuki_main/core.py
@@ -95,7 +95,7 @@ async def _(bot: T_Bot, event: T_MessageEvent):
     logs = repo.index
     reply = "Liteyuki updated!\n"
     reply += f"```\n{logs}\n```\n"
-    btn_restart = md.button(ulang.get("liteyuki.restart_now"), "restart-liteyuki")
+    btn_restart = md.button(ulang.get("liteyuki.restart_now"), "reload-liteyuki")
     pip.main(["install", "-r", "requirements.txt"])
     reply += f"{ulang.get('liteyuki.update_restart', RESTART=btn_restart)}"
     await md.send_md(reply, bot, event=event, at_sender=False)
