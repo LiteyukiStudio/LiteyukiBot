9bfe173f92abc5f88f86463a9a7dc18fd9ca6e6f
snowy <snowykami@outlook.com>
2024-04-05 20:07:33 +0800

    fix: typo of `config.yaml` -> `config.yml`

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -20,7 +20,7 @@ export default defineUserConfig({
                 name: "viewport",
                 content: "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no",
             },
-        ]
+        ],
     ],
 
 
