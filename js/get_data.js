abea5cd28f669f6f785428a4fccea9565576387a
snowykami <snowykami@outlook.com>
2024-08-01 05:11:58 +0000

    Deploying to gh-pages from @ LiteyukiStudio/LiteyukiBot@5d22f20ce35ad378f51237e118bfadf39a542eec 🚀

--- js/get_data.js
+++ js/get_data.js
@@ -1,27 +1,39 @@
+// 定义全局变量来存储数据
+let globalTotal = 0;
+let globalOnline = 0;
 
-function updatePageData() {
-  fetch("https://api.liteyuki.icu/count")
-    .then(res => res.json())
-    .then(data => {
-      let total = document.getElementById("total");
-      if(total !== null) {
-        total.innerText = data.register;
-      }
-    })
-    .catch(err => console.error(err));
-
-  fetch("https://api.liteyuki.icu/online")
-    .then(res => res.json())
-    .then(data => {
-        let online = document.getElementById("online");
-        if(online !== null) {
-            online.innerText = data.online;
-        }
-    })
-    .catch(err => console.error(err));
+// 从API获取数据并更新全局变量
+function fetchAndUpdateData() {
+    Promise.all([
+        fetch("https://api.liteyuki.icu/count").then(res => res.json()),
+        fetch("https://api.liteyuki.icu/online").then(res => res.json())
+    ])
+        .then(([countRes, onlineRes]) => {
+            globalTotal = countRes.register;
+            globalOnline = onlineRes.online;
+        })
+        .catch(err => {
+            console.error("Error fetching data:", err);
+        });
 }
 
-updatePageData();
-setInterval(() => {
-  updatePageData();
-}, 1000);
+// 更新页面显示，使用全局变量中的数据
+function updatePageDisplay() {
+    let countInfo = document.getElementById("count-info");
+    if (!countInfo) {
+        let info = `<div id="count-info" style="text-align: center; font-size: 20px; font-weight: 500">
+            全球实例:<span id="total">${globalTotal}</span>&nbsp;&nbsp;&nbsp;&nbsp;当前在线:<span id="online">${globalOnline}</span></div>`;
+        let mainDescription = document.querySelector("#main-description");
+        if (mainDescription) {
+            mainDescription.insertAdjacentHTML('afterend', info);
+        }
+        // 如果countInfo存在，可以在这里添加逻辑来更新现有的DOM元素
+    }
+}
+
+// 初始调用更新数据
+fetchAndUpdateData();
+
+// 设置定时器，分别以不同频率调用更新数据和更新页面的函数
+setInterval(fetchAndUpdateData, 10000); // 每10秒更新一次数据
+setInterval(updatePageDisplay, 1000); // 每1秒更新一次页面显示
