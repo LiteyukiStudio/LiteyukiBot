5d22f20ce35ad378f51237e118bfadf39a542eec
snowy <snowykami@outlook.com>
2024-08-01 13:11:05 +0800

    :memo: 新增全球统计

--- docs/.vuepress/client.js
+++ docs/.vuepress/client.js
@@ -3,6 +3,7 @@ import {defineClientConfig} from "vuepress/client";
 import resourceStoreComp from "./components/ResStore.vue";
 import pluginStoreComp from "./components/PluginStore.vue";
 import dashComp from "./components/Dash.vue";
+import homeComp from "./components/Home.vue";
 
 
 import ElementPlus from 'element-plus';
@@ -10,6 +11,7 @@ import ElementPlus from 'element-plus';
 
 export default defineClientConfig({
     enhance: ({app, router, siteData}) => {
+        app.component("homeComp", homeComp);
         app.component("dashComp", dashComp);
         app.component("resourceStoreComp", resourceStoreComp);
         app.component("pluginStoreComp", pluginStoreComp);
