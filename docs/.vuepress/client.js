2b537d27ec582ad5d041bfe6142379812db4103e
snowy <snowykami@outlook.com>
2024-08-05 06:02:53 +0800

    :memo: 新增全球统计

--- docs/.vuepress/client.js
+++ docs/.vuepress/client.js
@@ -1,5 +1,4 @@
 import {defineClientConfig} from "vuepress/client";
-import { defineEChartsConfig } from "vuepress-plugin-md-enhance/client";
 
 import resourceStoreComp from "./components/ResStore.vue";
 import pluginStoreComp from "./components/PluginStore.vue";
@@ -8,17 +7,6 @@ import homeComp from "./components/Home.vue";
 import geoComp from "./components/Geo.vue";
 
 // import ElementPlus from 'element-plus';
-
-defineEChartsConfig({
-  options: {
-    // 全局 ECharts 配置
-  },
-  setup: async () => {
-    // ECharts 设置
-    // 例如: await import("echarts-wordcloud")
-  },
-});
-
 
 export default defineClientConfig({
     enhance: ({app, router, siteData}) => {
--- docs/other/README.md
+++
@@ -1,8 +0,0 @@
----
-title: 其他
-index: false
-icon: question-circle
-category: 其他
----
-
-<Catalog />
