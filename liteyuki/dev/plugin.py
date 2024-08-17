78810d2ca8b3a1d914984c4ccf094befa34fa25e
snowy <snowykami@outlook.com>
2024-08-18 05:19:52 +0800

    :sparkles: 新增开发者模式，快速运行插件

--- liteyuki/dev/plugin.py
+++ liteyuki/dev/plugin.py
@@ -21,6 +21,8 @@ def run_plugins(*module_path: str | Path):
         module_path: 插件路径，参考`liteyuki.load_plugin`的函数签名
     """
     cfg = load_config_in_default()
-    cfg["liteyuki.plugins"] = cfg.get("liteyuki.plugins", []).extend(module_path)
+    plugins = cfg.get("liteyuki.plugins", [])
+    plugins.extend(module_path)
+    cfg["liteyuki.plugins"] = plugins
     bot = LiteyukiBot(**cfg)
     bot.run()
