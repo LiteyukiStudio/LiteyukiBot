20ad8dc53fef97ad2c3929094b29c3c933187774
snowykami <snowykami@outlook.com>
2024-10-19 21:15:37 +0800

    :bug: hotfix: from mypy import

--- liteyuki/session/__init__.py
+++ liteyuki/session/__init__.py
@@ -3,10 +3,9 @@
 该模块参考并引用了nonebot-plugin-alconna的消息段定义
 """
 from typing import Any
+from typing import Iterable
 
 from magicoca import Chan, select
-from mypy.server.objgraph import Iterable
-from six import Iterator
 
 
 def message_handler_thread(i_chans: Iterable[Chan[Any]]):
