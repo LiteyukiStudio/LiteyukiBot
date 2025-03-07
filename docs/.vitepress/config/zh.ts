7b97210a31760f4a1a096c83c8fb9eed7af9dfe4
Snowykami <snowykami@outlook.com>
2025-03-07 17:21:01 +0800

    📝 更新文档页脚信息，添加网站部署链接

--- docs/.vitepress/config/zh.ts
+++ docs/.vitepress/config/zh.ts
@@ -20,7 +20,7 @@ export const zh = defineConfig({
             '在 GitHub 上编辑此页',
         ),
         footer: {
-            message: '页面由 <a href="https://cdn.liteyuki.icu" target="_blank">Liteyukiflare CDN</a> 提供加速服务<br>文档由 <a href="https://vitepress.dev/">VitePress</a> 构建 | API引用由 <a href="https://github.com/LiteyukiStudio/litedoc">litedoc</a> 生成',
+            message: '网站部署在 <a href="https://meli.liteyuki.icu" target="_blank">Liteyukiflare Meli</a> 由 <a href="https://cdn.liteyuki.icu" target="_blank">Liteyukiflare CDN</a> 提供加速服务<br>文档由 <a href="https://vitepress.dev/">VitePress</a> 构建 | API引用由 <a href="https://github.com/LiteyukiStudio/litedoc">litedoc</a> 生成',
             copyright: ThemeConfig.copyright
         },
         outline: ThemeConfig.getOutLine("页面内容"),
